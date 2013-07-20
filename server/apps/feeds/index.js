d3c636f464f79e09363d5ae37445380e5d38a415
Tajima Junpei <p.baleine@gmail.com>
2013-07-20 12:21:52 +0900

    update feeds' create to refrect models

--- server/apps/feeds/index.js
+++ server/apps/feeds/index.js
@@ -6,6 +6,7 @@
 var express = require("express"),
     flash = require("connect-flash"),
     getFeedMetaData = require("./get-feed-meta-data"),
+    User = require("../../models/user"),
     Feed = require("../../models/feed");
 
 var app = module.exports = express();
@@ -23,15 +24,24 @@ app.use(flash());
  * GET /feeds
  */
 
-app.get("/feeds", function(req, res) {
-  Feed.find({
-    _user: req.session.user_id
-  }, function(err, feeds) {
-    res.render("index", {
-      bodyId: "feeds",
-      feeds: feeds
+app.get("/feeds", function(req, res, next) {
+  User.findOne({ _id: req.session.user_id })
+    .populate("_subscribes")
+    .exec(function(err, user) {
+      if (err) { next(err); }
+      res.render("index", {
+        bodyId: "feeds",
+        feeds: user._subscribes
+      });
     });
-  });
+  // Feed.find({
+  //   _user: req.session.user_id
+  // }, function(err, feeds) {
+  //   res.render("index", {
+  //     bodyId: "feeds",
+  //     feeds: feeds
+  //   });
+  // });
 });
 
 /**
@@ -39,20 +49,26 @@ app.get("/feeds", function(req, res) {
  */
 
 app.post("/feeds", function(req, res, next) {
-  var feed = new Feed(req.body),
-      url = req.body.url;
+  var url = req.body.url;
 
   if (!url) { throw new Error("url is not specified"); }
 
+  // async!!
+
   getFeedMetaData(url, function(err, meta) {
     if (err) { return next(err); }
-
-    feed._user = req.session.user_id;
-    feed.name = meta[0].title;
-    feed.url = meta[0].link;
-    feed.save(function(err) {
+    
+    User.findOne({ _id: req.session.user_id }, function(err, user) {
       if (err) { return next(err); }
-      res.send(feed);
+      var feed = new Feed(meta[0]);
+      feed.save(function(err) {
+        if (err) { return next(err); }
+        user._subscribes.push(feed._id);
+        user.save(function(err) {
+          if (err) { next(err); }
+          res.send(feed);
+        });
+      });
     });
   });
 });
