e12445673133a07e5c6aede4fad5d50800539496
Tajima Junpei <p.baleine@gmail.com>
2013-07-24 09:22:06 +0900

    remove comment

--- server/apps/posts/index.js
+++ server/apps/posts/index.js
@@ -19,27 +19,7 @@ app.use(express.bodyParser());
  */
 
 app.get("/feeds/:fid/posts", loadFeed, function(req, res) {
-  // load feeds
-  //  if feeds of fid is not users' one then throw err
   if (!req.feed) { return res.send(404); }
-
-  // var Post = require("../../models/post");
-
-  // request("http://feeds.feedburner.com/dailyjs")
-  //   .pipe(new FeedParser)
-  //   .on("data", function(article) {
-  //     var post = new Post(article);
-  //     post._id = article.guid;
-  //     post.imageUrl = article.image.url;
-  //     post.imageTitle = article.image.title;
-  //     post.save(function(err, post) {
-  //       console.log(err);
-  //       console.log(post);
-  //     });
-  //   });
-
-  // console.log(req);
-  console.log(req.feed._feed_posts);
   res.send(req.feed._feed_posts);
 });
 
