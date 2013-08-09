d4734d452fc1b89047ed003670a1406ae35d21a0
Tajima Junpei <p.baleine@gmail.com>
2013-08-09 15:25:11 +0900

    refactor

--- migrations/001-add-unread-count.js
+++ migrations/001-add-unread-count.js
@@ -13,31 +13,27 @@ exports.up = function(next) {
   User.find(function(err, users) {
     if (err) { throw err; }
 
-    var tasks = [];
-
     if (!users) { return next(); }
 
-    users.forEach(function(user) {
-      tasks.push(function(cb) {
-        // skip if target user dose not have `subscribe` field
-        if (!user.get('_subscribes')) { return cb(null); }
+    function updateUserSubscribe(user, cb) {
+      // skip if target user dose not have `subscribe` field
+      if (!user.get('_subscribes')) { return cb(null); }
 
-        debug("migrate " + user.name + "'s subscribes");
+      debug("migrate " + user.name + "'s subscribes");
 
-        var subscribes = user.get('_subscribes').map(function(subscribe) {
-          return { _feed: subscribe, unread_count: 0 };
-        });
-
-        // to delete filed that current User dose not have,
-        // call native collection's update.
-        User.collection.update({ _id: user._id }, {
-          $set: { subscribes: subscribes },
-          $unset: { _subscribes: 1 }
-        }, cb);
+      var subscribes = user.get('_subscribes').map(function(subscribe) {
+        return { _feed: subscribe, unread_count: 0 };
       });
-    });
 
-    async.series(tasks, next);
+      // to delete filed that current User dose not have,
+      // call native collection's update.
+      User.collection.update({ _id: user._id }, {
+        $set: { subscribes: subscribes },
+        $unset: { _subscribes: 1 }
+      }, cb);
+    }
+
+    batch(users, updateUserSubscribe, next);
   });
 };
 
@@ -45,25 +41,37 @@ exports.down = function(next) {
   User.find(function(err, users) {
     if (err) { throw err; }
 
-    var tasks = [];
-
     if (!users) { return next(); }
 
-    users.forEach(function(user) {
-      tasks.push(function(cb) {
-        if (!user.get('subscribes')) { return cb(null); }
+    function updateUserSubscribe(user, cb) {
+      if (!user.get('subscribes')) { return cb(null); }
         
-        var _subscribes = user.get('subscribes').map(function(subscribe) {
-          return subscribe._feed;
-        });
-
-        User.collection.update({ _id: user._id }, {
-          $set: { _subscribes: _subscribes },
-          $unset: { subscribes: 1 }
-        }, cb);
+      var _subscribes = user.get('subscribes').map(function(subscribe) {
+        return subscribe._feed;
       });
-    });
 
-    async.series(tasks, next);
+      User.collection.update({ _id: user._id }, {
+        $set: { _subscribes: _subscribes },
+        $unset: { subscribes: 1 }
+      }, cb);
+    }
+
+    batch(users, updateUserSubscribe, next);
   });
 };
+
+/**
+ * Batch execute users update.
+ */
+
+function batch(users, update, next) {
+  var tasks = [];
+
+  users.forEach(function(user) {
+    tasks.push(function(cb) {
+      update(user, cb);
+    });
+  });
+
+  async.series(tasks, next);
+}
